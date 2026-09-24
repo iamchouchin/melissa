08c6836c3914fd6f5f5a4444166bc6b66e773598
Melissa <mgalvan@outlook.fr>
2026-09-24 15:56:28 +0200

    Add the “roles” property

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -25,6 +25,9 @@ class User
     #[ORM\Column(length: 255)]
     private ?string $password = null;
 
+    #[ORM\Column]
+    private array $roles = [];
+
     public function getId(): ?int
     {
         return $this->id;
@@ -74,6 +77,21 @@ class User
     public function setPassword(string $password): static
     {
         $this->password = $password;
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
 
         return $this;
     }
