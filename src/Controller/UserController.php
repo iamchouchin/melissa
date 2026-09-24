e235ef4d5e52c1bc5fb727bcdb547306e0541925
Melissa <mgalvan@outlook.fr>
2026-09-25 00:00:44 +0200

    get the logged-in user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    #[Route('/mon-compte', name: 'app_user')]
     public function index(): Response
     {
+        $user = $this->getUser();
+
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'user' => $user
         ]);
     }
 }
